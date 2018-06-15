c4e9c2d829bc915a33f712333b17664418ef937a
Yuan.TX <yuantianxin@126.com>
2018-06-15 22:36:04 +0800

    <A Simple Implementation of MPT>

---
+++ src/com/encoding/HexConver.java
@@ -0,0 +1,164 @@
+package com.encoding;
+
+import java.util.Locale;
+
+public class HexConver {
+    private final static char[] mChars = "0123456789ABCDEF".toCharArray();
+    private final static String mHexStr = "0123456789ABCDEF";
+    /**
+     * 检查16进制字符串是否有效
+     * @param sHex String 16进制字符串
+     * @return boolean
+     */
+    public static boolean checkHexStr(String sHex){
+        String sTmp = sHex.trim().replace(" ", "").toUpperCase(Locale.US);
+        int iLen = sTmp.length();
+
+        if (iLen > 1 && iLen%2 == 0){
+            for(int i=0; i<iLen; i++)
+                if (!mHexStr.contains(sTmp.substring(i, i+1)))
+                    return false;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    /**
+     * 字符串转换成十六进制字符串
+     * @param str String 待转换的ASCII字符串
+     * @return String 每个Byte之间空格分隔，如: [61 6C 6B]
+     */
+    public static String str2HexStr(String str){
+        StringBuilder sb = new StringBuilder();
+        byte[] bs = str.getBytes();
+
+        for (byte b : bs) {
+            sb.append(mChars[(b & 0xFF) >> 4]);
+            sb.append(mChars[b & 0x0F]);
+            sb.append(' ');
+        }
+        return sb.toString().trim();
+    }
+
+    /**
+     * 十六进制字符串转换成 ASCII字符串
+     */
+    public static String hexStr2Str(String hexStr){
+        hexStr = hexStr.trim().replace(" ", "").toUpperCase(Locale.US);
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int iTmp;
+
+        for (int i = 0; i < bytes.length; i++){
+            iTmp = mHexStr.indexOf(hexs[2 * i]) << 4;
+            iTmp |= mHexStr.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (iTmp & 0xFF);
+        }
+        return new String(bytes);
+    }
+
+    /**
+     * bytes转换成十六进制字符串
+     * @param b byte[] byte数组
+     * @param iLen int 取前N位处理 N=iLen
+     * @return String 每个Byte值之间空格分隔
+     */
+    public static String byte2HexStr(byte[] b, int iLen){
+        StringBuilder sb = new StringBuilder();
+        for (int n=0; n<iLen; n++){
+            sb.append(mChars[(b[n] & 0xFF) >> 4]);
+            sb.append(mChars[b[n] & 0x0F]);
+            sb.append(' ');
+        }
+        return sb.toString().trim().toUpperCase(Locale.US);
+    }
+
+    /**
+     * bytes字符串转换为Byte值
+     * @param src String Byte字符串，每个Byte之间没有分隔符(字符范围:0-9 A-F)
+     * @return byte[]
+     */
+    public static byte[] hexStr2Bytes(String src){
+        /*对输入值进行规范化整理*/
+        src = src.trim().replace(" ", "").toUpperCase(Locale.US);
+        //处理值初始化
+        int m,n;
+        int iLen=src.length()/2; //计算长度
+        byte[] ret = new byte[iLen]; //分配存储空间
+
+        for (int i = 0; i < iLen; i++){
+            m=i*2+1;
+            n=m+1;
+            ret[i] = (byte)(Integer.decode("0x"+ src.substring(i*2, m) + src.substring(m,n)) & 0xFF);
+        }
+        return ret;
+    }
+
+    /**
+     * String的字符串转换成unicode的String
+     */
+    public static String strToUnicode(String strText) {
+        char c;
+        StringBuilder str = new StringBuilder();
+        int intAsc;
+        String strHex;
+        for (int i = 0; i < strText.length(); i++){
+            c = strText.charAt(i);
+            intAsc = (int) c;
+            strHex = Integer.toHexString(intAsc);
+            if (intAsc > 128)
+                str.append("\\u");
+            else // 低位在前面补00
+                str.append("\\u00");
+            str.append(strHex);
+        }
+        return str.toString();
+    }
+
+    /**
+     * unicode的String转换成String的字符串
+     */
+    public static String unicodeToString(String hex){
+        int t = hex.length() / 6;
+        int iTmp;
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < t; i++){
+            String s = hex.substring(i * 6, (i + 1) * 6);
+            // 将16进制的string转为int
+            iTmp = (Integer.valueOf(s.substring(2, 4), 16) << 8) | Integer.valueOf(s.substring(4), 16);
+            // 将int转换为字符
+            str.append(new String(Character.toChars(iTmp)));
+        }
+        return str.toString();
+    }
+
+    /**将二进制转换成16进制
+     */
+    public static String parseByte2HexStr(byte buf[]) {
+        StringBuilder sb = new StringBuilder();
+        for (byte aBuf : buf) {
+            String hex = Integer.toHexString(aBuf & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    /**将16进制转换为二进制
+     */
+    public static byte[] parseHexStr2Byte(String hexStr) {
+        if (hexStr.length() < 1)
+            return null;
+        byte[] result = new byte[hexStr.length()/2];
+        for (int i = 0;i< hexStr.length()/2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i*2, i*2+1), 16);
+            int low = Integer.parseInt(hexStr.substring(i*2+1, i*2+2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+
+}
---
+++ src/com/encoding/RLP.java
@@ -0,0 +1,236 @@
+package com.encoding;
+
+import java.util.Objects;
+
+public class RLP {
+
+    public static byte[] rlpEncoding(byte[][] input) {
+        byte[] output;
+        byte[] temp = new byte[0];
+
+        for (int i = 0; i < input.length; i++) {
+            if(i == 0) {
+                temp = rlpEncoding(input[i]);
+            }else {
+                byte[] temp2 = rlpEncoding(input[i]);
+                byte[] temp3 = temp.clone();
+
+                temp = new byte[temp3.length + temp2.length];
+                System.arraycopy(temp3,0,temp,0,temp3.length);
+                System.arraycopy(temp2,0,temp,temp3.length,temp2.length);
+            }
+        }
+
+        byte[] prefix = encode_length(temp.length,192);
+        output = new byte[Objects.requireNonNull(prefix).length + temp.length];
+        System.arraycopy(prefix,0,output,0,prefix.length);
+        System.arraycopy(temp,0,output,prefix.length,temp.length);
+
+        return output;
+    }
+
+    public static byte[][] rlpDecoding(byte[] input) {
+        byte[][] output;
+        int prefixLength;
+        int length;
+
+        if(((input[0] & 0xFF) >= 0xc0) && ((input[0] & 0xFF) <= 0xf7)) {
+            prefixLength = 1;
+            length = (int)input[0] - 0xc0;
+        }else if((input[0] & 0xFF) >= 0xf8) {
+            prefixLength = (input[0] & 0xFF) - 0xf7 + 1;
+            length = 0;
+            for(int i = 1; i < prefixLength; i++) {
+                length = 256 * length + (input[i] & 0xFF);
+            }
+        }else return null;
+
+        int i = prefixLength;
+        int index = 0;
+        byte[][] temp = new byte[20][];
+
+        while( i < (prefixLength + length) ) {
+            int l = getStrLength(input,i);
+            temp[index] = rlpDecodingStr(input,i);
+            index++;
+            i = i + l;
+        }
+
+        output = new byte[index][];
+        System.arraycopy(temp,0,output,0,index);
+        return output;
+    }
+
+    /**
+     * Decoding
+     * Decode A Single String Inside A List
+     * created in 22:21 2018/6/15
+     */
+    private static byte[] rlpDecodingStr(byte[] input, int index) {
+        byte[] output;
+
+        if((input[index] & 0xFF) < 0x80) {
+            output = new byte[1];
+            output[0] = input[index];
+
+            return output;
+
+        }else if((input[index] & 0xFF) == 0x80) {
+
+            return null;
+
+        }else if(((input[index] & 0xFF) > 0x80) && ((input[index] & 0xFF) <= 0xb7)) {
+            int length = (input[index] & 0xFF) - 0x80;
+            output = new byte[length];
+
+            System.arraycopy(input,index + 1,output,0,length);
+
+            return output;
+
+        }else if(((input[index] & 0xFF) >= 0xb8) && ((input[index] & 0xFF) <= 0xbf)) {
+            int byteLength = (input[index] & 0xFF) - 0xb7;
+            int length = 0;
+
+            for(int i = 0; i < byteLength; i++) {
+                length = 256 * length + (input[index+i+1] & 0xFF);
+            }
+            output = new byte[length];
+
+            System.arraycopy(input,1+byteLength+index,output,0,length);
+
+            return output;
+
+        }else
+            return null;
+    }
+
+    /**
+     * Decoding
+     * Get Length For A Single String Inside A List
+     * created in 22:19 2018/6/15
+     */
+    private static int getStrLength(byte[] input, int index) {
+        int length;
+
+        if((input[index] & 0xFF) < 0x80) {
+            length = 1;
+            return length;
+
+        }else if((input[index] & 0xFF) == 0x80) {
+            length = 0;
+            return length + 1;
+
+        }else if(((input[index] & 0xFF) > 0x80) && ((input[index] & 0xFF) <= 0xb7)) {
+            length = (input[index] & 0xFF) - 0x80;
+            return length + 1;
+
+        }else if(((input[index] & 0xFF) >= 0xb8) && ((input[index] & 0xFF) <= 0xbf)) {
+            int byteLength = (input[index] & 0xFF) - 0xb7;
+            length = 0;
+
+            for(int i = index; i < index + byteLength; i++) {
+                length = 256 * length + (input[i+1] & 0xFF);
+            }
+            return length + byteLength + 1;
+
+        }else
+            return -1;
+    }
+
+    /**
+     * Encoding
+     * RLP Encoding with A One-dimension Byte Array as Input
+     * created in 17:56 2018/6/15
+     */
+    private static byte[] rlpEncoding(byte[] input) {
+        if(input == null) {
+            byte[] output = new byte[1];
+            output[0] = (byte)0x80;
+
+            return output;
+        }else if((input.length == 1) && ((int)input[0] < 0x80)) {
+
+            return input;
+        }else {
+            byte[] output;
+            byte[] prefix = encode_length(input.length, 128);
+
+            output = new byte[Objects.requireNonNull(prefix).length + input.length];
+            System.arraycopy(prefix,0,output,0,prefix.length);
+            System.arraycopy(input,0,output,prefix.length,input.length);
+
+            return output;
+        }
+    }
+
+    /**
+     * Encoding
+     * Add Prefix For Byte Array
+     * created in 11:42 2018/6/13
+     */
+    private static byte[] encode_length(int L, int offset) {
+        byte[] output;
+
+        if(L < 56) {
+            output = new byte[1];
+            output[0] = (byte)(L + offset);
+
+            return output;
+        }else if(L < Math.pow(0xFF, 8)) {
+            byte[] temp = byteLength(L);
+
+            output = new byte[1 + temp.length];
+            output[0] = (byte)(temp.length + offset + 55);
+            System.arraycopy(temp,0,output,1,temp.length);
+            
+            return output;
+        }else {
+            
+            return null;
+        }
+    }
+
+    /**
+     * Encoding
+     * Divide Integer Length into A Byte Array
+     * created in 11:44 2018/6/13
+     */
+    private static byte[] byteLength(int x) {
+
+        int byteLength = 0;
+        byte[] temp = new byte[8];
+
+        while(x != 0) {
+            temp[byteLength] = (byte)(x & 0xFF);
+            x = x / 256;
+            byteLength++;
+        }
+
+        byte[] result = new byte[byteLength];
+        System.arraycopy(temp, 0, result, 0, result.length);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        byte[][] shit = new byte[3][];
+        shit[0] = "吔屎啦，梁非凡".getBytes();
+        shit[1] = "我要草你啊".getBytes();
+        shit[2] = "不是你草我，是我要草你啊".getBytes();
+
+        System.out.println(HexConver.byte2HexStr(shit[0],shit[0].length));
+        System.out.println(HexConver.byte2HexStr(shit[1],shit[1].length));
+        System.out.println(HexConver.byte2HexStr(shit[2],shit[2].length));
+
+        byte[] k = rlpEncoding(shit);
+        System.out.println(HexConver.byte2HexStr(k,k.length));
+        System.out.println(k.length);
+
+        byte[][] l = rlpDecoding(k);
+        System.out.println(Objects.requireNonNull(l).length);
+        System.out.println(HexConver.byte2HexStr(l[0],l[0].length));
+        System.out.println(HexConver.byte2HexStr(l[1],l[1].length));
+        System.out.println(HexConver.byte2HexStr(l[2],l[2].length));
+    }
+
+}
