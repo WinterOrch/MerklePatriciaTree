35108c355d0ce34c97444730e5ac4e392a81467c
xxxmmc <857841655@qq.com>
2018-06-16 13:48:07 +0800

    12313321

---
+++ src/com/system/Extension_Node.java
@@ -0,0 +1,6 @@
+package com.system;
+
+public class Extension_Node {
+
+
+}
--- src/com/system/Trie.java
+++ src/com/system/Trie.java
@@ -1,5 +1,10 @@
 package com.system;
 
 public class Trie {
-    
+    public void constructTree(Leaf Node)
+    {
+
+    }
+
+
 }
